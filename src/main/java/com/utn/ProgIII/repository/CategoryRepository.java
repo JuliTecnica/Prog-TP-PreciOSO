80655d6079c57b54afdf623666a8b6e34a5092f8
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-04-08 11:37:55 -0300

    (add) add category method to service

--- src/main/java/com/utn/ProgIII/repository/CategoryRepository.java
+++ src/main/java/com/utn/ProgIII/repository/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.utn.ProgIII.repository;
 
-import com.utn.ProgIII.model.Credential.Credential;
+import com.utn.ProgIII.model.Product.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CategoryRepository extends JpaRepository<Credential,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
     boolean existsByName(String name);
 }
--- src/main/java/com/utn/ProgIII/service/implementations/CategoryServiceImpl.java
+++ src/main/java/com/utn/ProgIII/service/implementations/CategoryServiceImpl.java
@@ -1,18 +1,29 @@
 package com.utn.ProgIII.service.implementations;
 
 import com.utn.ProgIII.dto.CategoryDTO;
+import com.utn.ProgIII.mapper.CategoryMapper;
+import com.utn.ProgIII.model.Product.Category;
 import com.utn.ProgIII.repository.CategoryRepository;
 import com.utn.ProgIII.service.interfaces.CategoryService;
+import com.utn.ProgIII.validations.CategoryValidations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Autowired
+    private CategoryValidations categoryValidations;
 
     @Override
     public CategoryDTO addCategory(CategoryDTO categoryDTO) {
+        Category category = categoryMapper.toEntity(categoryDTO);
+        categoryValidations.checkIfCategoryNameExists(category);
 
+        Category saved = categoryRepository.save(category);
+        return categoryMapper.toDTO(saved);
     }
 
     @Override

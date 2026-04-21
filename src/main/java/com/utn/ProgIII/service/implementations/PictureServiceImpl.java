941c0714cfd5ba19055fa8060d6bd2155eb7f03b
JuliTecnica <71672591+JuliTecnica@users.noreply.github.com>
2026-04-21 15:36:00 -0300

    (add) protections against undesired file types in picture service

--- src/main/java/com/utn/ProgIII/service/implementations/PictureServiceImpl.java
+++ src/main/java/com/utn/ProgIII/service/implementations/PictureServiceImpl.java
@@ -1,5 +1,6 @@
 package com.utn.ProgIII.service.implementations;
 
+import com.utn.ProgIII.exceptions.BadRequestException;
 import com.utn.ProgIII.exceptions.InternalServerError;
 import com.utn.ProgIII.exceptions.NotFoundException;
 import com.utn.ProgIII.service.interfaces.PictureService;
@@ -12,13 +13,29 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class PictureServiceImpl implements PictureService {
 
+    private final List<String> ALLOWED_FILE_EXTENSIONS = Arrays.asList(".jpg", ".png", ".webp", ".jpeg");
+    private final List<String> ALLOWED_FILE_TYPES = Arrays.asList("image/jpeg","image/png");
+
     @Override
     public String uploadPicture(String path, MultipartFile file) {
         String extension = "." + StringUtils.getFilenameExtension(file.getOriginalFilename());
+
+        if(!ALLOWED_FILE_TYPES.contains(file.getContentType()))
+        {
+            throw new BadRequestException("Los tipos de archivos permitidos son " + String.join(", ", ALLOWED_FILE_EXTENSIONS.stream().map(String::valueOf).toList()));
+        }
+
+        if(!ALLOWED_FILE_EXTENSIONS.contains(extension))
+        {
+            throw new BadRequestException("Los tipos de archivos permitidos son " + String.join(", ", ALLOWED_FILE_EXTENSIONS.stream().map(String::valueOf).toList()));
+        }
+
         String filename = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new java.util.Date()) + extension;
 
 
